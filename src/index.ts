b70b9ad069bda0c30f6697e4a1a98e7e8b6621b5
GFrancV <gfrancv@hotmail.com>
2024-05-17 17:26:50 +0100

    chore: Update package.json and typescript configuration

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,4 @@
 import { getPlaylist, getTrackInfo, searchTrack, searchPlaylist } from "./search";
 import { validateID } from "./tools";
 
-class SpotifySearch {
-	searchTrack(query: string) {
-		return searchTrack(query);
-	}
-
-	searchPlaylist(query: string) {
-		return searchPlaylist(query);
-	}
-
-	getTrackInfoById(id: string) {
-		return getTrackInfo(id);
-	}
-
-	getPlaylist(id: string) {
-		return getPlaylist(id);
-	}
-
-	validateID(id: string) {
-		return validateID(id);
-	}
-}
-
-const spotifySearch = new SpotifySearch();
-
-export default spotifySearch;
+export { getPlaylist, getTrackInfo, searchTrack, searchPlaylist, validateID };
